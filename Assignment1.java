341b44c2ca24ade052548b457b282b304320719d
Ramir D. <ramirdalencour@gmail.com>
2024-01-11 01:07:27 -0500

    created the Java file

---
+++ Assignment1.java
@@ -0,0 +1,10 @@
+
+public class Assignment1
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println("How many days are there??");
+        
+    }
+}
