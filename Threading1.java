e2cad201439b0a1da88f01491736b0143bd770e8
Ramir D. <ramirdalencour@gmail.com>
2024-01-15 22:09:57 -0500

    Exploring some ideas

--- Threading1.java
+++ Threading1.java
@@ -1,10 +1,22 @@
 // Ramir Dalencour
 
-public class Threading1
+public class Threading1 extends Thread
 {
     public static void main(String [] args)
     {
         System.out.println("Hello, world!");
         System.out.println("How are you doing??");
     }
+
+
+    // Creating the new threads
+    Thread myThread1 = new Thread();
+    Thread myThread2 = new Thread();
+    Thread myThread3 = new Thread();
+    Thread myThread4 = new Thread();
+    Thread myThread5 = new Thread();
+    Thread myThread6 = new Thread();
+    Thread myThread7 = new Thread();
+    Thread myThread8 = new Thread();
+
 }
